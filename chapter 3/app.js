2d9e73dc218bde24e3ffe316a1d369e49185a2c0
Haaniya-ismail186 <haaniyaismail186@gmail.com>
2025-12-29 03:39:27 +0500

    First commit

---
+++ chapter 3/app.js
@@ -0,0 +1,16 @@
+var age = "I am 17 Years old";
+console.log(age);
+alert(age);
+
+var visit = " 14 ";
+document.write("You have visited this site " + visit + " times<br>");
+
+var birthYear = "2008";
+document.write("My birth year is " + birthYear + "<br>");
+document.write("Data type of my dclared variable is number <br>");
+
+var customerName = "Haaniya Ismail";
+var customerShirt = "5 T-shirts";
+var customerStore = "XYZ clothing store";
+
+document.write("<b>" + customerName + "</b>" + " ordered " + "<b>" + customerShirt + "</b>" + " on " + customerStore);
