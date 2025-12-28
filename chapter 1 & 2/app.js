bb05de9c0d8acaf14b7645bd7f2f3ac308e5855a
Haaniya-ismail186 <haaniyaismail186@gmail.com>
2025-12-29 01:53:01 +0500

    First commit

--- chapter 1 & 2/app.js
+++ chapter 1 & 2/app.js
@@ -4,3 +4,53 @@ alert("Welcome to JS Land...\nHappy Coding!");
 alert("Welcome to JS Land...");
 alert("Happy Coding!");
 alert("Hello... I can run JS through my web browser's console");
+
+
+// 1. Username
+var userName = "Haaniya Ismail";
+console.log(userName);
+document.write(userName + "<br><br>");
+
+// 2. Message
+var message = "Hello World";
+console.log(message);
+document.write(message + "<br><br>");
+alert(message);
+
+// 3. Student Name
+var stdName = "Haaniya Ismail";
+console.log(stdName);
+document.write(stdName + "<br><br>");
+alert(stdName);
+
+// 4. Student Age
+var stdAge = "15 years old";
+console.log(stdAge);
+document.write(stdAge + "<br><br>");
+alert(stdAge);
+
+// 5. Student Course
+var stdCourse = "Certified Mobile Application Development";
+console.log(stdCourse);
+document.write(stdCourse + "<br><br>");
+alert(stdCourse);
+
+// 6. Pizza pattern
+var foodName = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+console.log(foodName);
+alert(foodName);
+
+// 7. Email
+var email = "haaniismail8@gmail.com";
+console.log(email);
+alert("My email address is " + email);
+
+// 8. Book
+var book = "A smarter way to learn JavaScript";
+console.log(book);
+alert("I am trying to learn from the Book " + book);
+
+// 9. Final message
+document.write("Yah! I can write HTML content through JavaScript");
+
+
